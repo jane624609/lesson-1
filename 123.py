4df1eda78593e564e4358afa0b370daf85b20edc
Jane <jane624609@gmail.com>
2019-12-09 13:39:23 +0800

    Add new file

---
+++ 123.py
@@ -0,0 +1,12 @@
+price = 10
+pen = 0.38
+rain =True
+name = 'jane'
+print(price,pen,rain,name)
+
+name = input('請輸入名字:')
+print('嗨', name)
+
+x = input ('請輸入身高')
+y = input ('請輸入體重')
+print ('你的身高:',x,'你的體重:',y)
